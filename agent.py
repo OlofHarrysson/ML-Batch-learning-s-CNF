f15d58f7a6322931b3354bf059297b5397884440
OlofHarrysson <harrysson.olof@gmail.com>
2017-03-25 13:02:32 +0100

    trying to implement with clauses as a model. running into problems on that a clause version can't make a decision given an observation. It can evaluate to true or false, not sure if thats enough

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import itertools
 from collections import Counter
+import math
 
 class Agent:
     def __init__(self, epsilon, delta):
@@ -15,7 +16,27 @@ class Agent:
     def compute_required_training_dataset_size(self):
         # TODO - compute your value here.
         # It should depend on self.n_variables and self.j, self.epsilon and self.delta.
-        return 100
+
+
+        # TODO - How does it depend on j?
+        # TODO - n in theorem 3.4 means number of versions right?
+        # n = self.n_variables
+        n = len(self.model)
+        e = self.epsilon
+        d = self.delta
+
+        m = n / e * math.log(n / d) # 2n = model length
+        m = int(m) + 1
+
+        # m = 1 / e * math.log(2 / d) # 2n unitsteps = 1? Tagen från http://www.cs.princeton.edu/courses/archive/spr06/cos511/scribe_notes/0214.pdf sida 2
+        # m = int(m) + 1
+
+
+        # print(m)
+        # print(len(self.model))
+        # sys.exit(1)
+        m = 10000
+        return m
 
     def process_first_observation(self, interpretation):
         # TODO: you can't check reward here. Anything else?
@@ -27,9 +48,12 @@ class Agent:
 
         if reward is not None:
 
-            if reward is 0: # Only change model if guess was wrong
+            print(self.oracle.getcnf())
+            print(interpretation)
+            pause()
 
-                for version in self.model[:]: # Iterate over a copy
+            if reward is 0: # Only change model if guess was wrong
+                for version in self.model[:]: # Iterate over a copy TODO copy?
                     if version[0] == tuple(self.prev_interp) and version[1] == self.prev_pred:
                         self.model.remove(version)
                         break
@@ -72,17 +96,41 @@ class Agent:
 # pause()
 
     def init_model(self):
-        permutation = list(itertools.product([True, False], repeat=self.n_variables))
-        model = []
-        for version in permutation:
-            model.append((version, False))
-            model.append((version, True))
+        # permutation = list(itertools.product([True, False], repeat=self.n_variables))
+        # model = []
+        # for version in permutation:
+        #     model.append((version, False))
+        #     model.append((version, True))
 
-        self.model = model
+        # self.model = model
+
+        n = 10
+        j = 9
+
+
+
+
+        perm = list(itertools.product([1, 0 ,None], repeat=n))
+        # perm = list(itertools.product([True, False, None, None, None], repeat=j)) # 5^3 = 125 -> j=repeat
+        # perm = list(itertools.permutations([0,1,None], 3))
+
+        copy = []
+        for clause in perm:
+            nbr_None = clause.count(None)
+            if nbr_None >= n - j and nbr_None < n:
+                copy.append(clause)
+
+        # print(self.oracle.getcnf())
+        # print(perm2)
+        print(copy)
+        print(len(copy))
+        # print(perm)
+        # print(len(perm))
+
+
+        print("n^j = " + str(n ** j))
+        sys.exit(1)
 
 
 def pause():
     programPause = input("Press the <ENTER> key to continue...")
-
-
-
